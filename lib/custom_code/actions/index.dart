c96e676169049b13cee42a631bb7afbbf2b6b87d
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-09-18 08:57:26 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/index.dart
+++ lib/custom_code/actions/index.dart
@@ -1,2 +1,3 @@
 export 'get_or_create_request.dart' show getOrCreateRequest;
 export 'print_to_console_action.dart' show printToConsoleAction;
+export 'can_open_supported_links.dart' show canOpenSupportedLinks;
